e7b058c30100de6525275022f6db034dea287389
zendrix396 <yzendric@gmail.com>
2026-05-11 19:08:17 +0530

    Clean repository

---
+++ config.py
@@ -0,0 +1,11 @@
+SEARCH_KEYWORDS = "Full Stack"
+LOCATION = "USA"
+PROFILES_TO_SCRAPE = 10
+# detailed, summary
+SCRAPE_MODE = "SUMMARY"
+
+HEADLESS = True
+
+OUTPUT_FILENAME = "demo.csv"
+COOKIES_FILENAME = "cookies.json"
+LOGIN_URL = "https://www.linkedin.com/login"
